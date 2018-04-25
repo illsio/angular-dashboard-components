587893be885acaffcd78e29b9e6b4956d4cbebe1
Till Degkwitz <till.degkwitz@hcu-hamburg.de>
2018-04-25 17:48:38 +0200

    Module configuration

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,3 @@
-import {LineComponent} from "./charts/line/line.component";
-import {PieComponent} from "./charts/pie/pie.component";
-import {WordCloudComponent} from "./charts/word-cloud/word-cloud.component";
-
+export {LineComponent} from "./charts/line/line.component";
+export {PieComponent} from "./charts/pie/pie.component";
+export {WordCloudComponent} from "./charts/word-cloud/word-cloud.component";
