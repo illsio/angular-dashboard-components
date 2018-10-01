14b4f26a0798e0d4cab9c0ce3bcc1b32882569dc
Till Degkwitz <till.degkwitz@hcu-hamburg.de>
2018-10-01 13:57:13 +0200

    ChartUtils Injectable entfernt

--- components/utils/chart.utils.ts
+++ components/utils/chart.utils.ts
@@ -1,7 +1,5 @@
-import {Injectable} from '@angular/core';
 import {DataFunctionEnum} from "../enum/data-function.enum";
 
-@Injectable()
 export class ChartUtils {
 
     constructor() {
--- dist/utils/chart.utils.js
+++ dist/utils/chart.utils.js
@@ -1,13 +1,3 @@
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-import { Injectable } from '@angular/core';
 import { DataFunctionEnum } from "../enum/data-function.enum";
 var ChartUtils = (function () {
     function ChartUtils() {
@@ -165,10 +155,6 @@ var ChartUtils = (function () {
         }
         return string.charAt(0).toUpperCase() + string.slice(1);
     };
-    ChartUtils = __decorate([
-        Injectable(),
-        __metadata("design:paramtypes", [])
-    ], ChartUtils);
     return ChartUtils;
 }());
 export { ChartUtils };
