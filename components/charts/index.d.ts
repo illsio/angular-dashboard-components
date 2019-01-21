7886905b0446ddb6af12f7ad6d9d03221b47efb8
Till Degkwitz <till.degkwitz@hcu-hamburg.de>
2019-01-21 16:35:37 +0100

    RollBack - Integration in CoSI Master vorerst gescheitert

--- components/charts/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "./line/line.component";
-export * from "./pie/pie.component";
-export * from "./word-cloud/word-cloud.component";
-export {DashComponentsModule} from "../dash-components.module";
